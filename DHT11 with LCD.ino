a0bb1f864ae982ddb90613ac901f0f5ff5cfcbbd
MuhammadMooazam <muhammadmoazam29@gmail.com>
2021-04-27 05:47:03 +0500

    added .ino files

---
+++ DHT11 with LCD.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+DHT dht(8, DHT11);
+
+void setup() {
+  dht.begin();
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %");
+  lcd.setCursor(0, 1);
+  lcd.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C");
+}
